c5d8ef7b9bca4a05e2ac573df0a39b7d62e1ac1a
노찬규 <cgnoh@thegolf.com>
2026-01-05 06:17:39 +0000

    gitignore 파일 추가 및 환경설정 수정/패키치 업데이트

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2026-01-05',
   devtools: { enabled: false },
   devServer: {
+    host: process.env.NUXT_HOST || '192.168.10.222',
+    port: Number(process.env.NUXT_PORT || 3000),
     hot: true,
   },
-  compatibilityDate: '2025-02-26',
   css: [
     '@/assets/scss/base/common.scss',
     '@/assets/scss/base/reset.scss',
     '@/assets/scss/base/variables.scss',
-  ]
+  ],
+  app: {
+    baseURL: '/',
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 300,
+        awaitWriteFinish: {
+          stabilityThreshold: 500,
+          pollInterval: 100
+        }
+      }
+    }
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://your-api-server.com',
+      recaptchaSitekey: process.env.RECAPTCHA_SITEKEY,
+      s3BucketUrl: process.env.NUXT_PUBLIC_S3_BUCKET_URL || "https://aishop.thegolf.com",
+      kakaoMapKey: process.env.NUXT_PUBLIC_KAKAO_KEY,
+    },
+  },
 })
